738b541e7712eeb4981caf92b7d5e7a46d9401fc
anmolaman20 <anmolaman8853@gmail.com>
2023-01-28 22:59:20 +0530

    introduction-to-pointer

---
+++ Intro.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+/*
+    Pointer is a special variable used to store the address of other variable;
+    Pointer variable is declared using * symbol
+    Pointer variable is dereferenced using * operator to get the content of the variable where our variable is pointing.
+
+
+    these are just the example but this is not actually use of pointer.
+*/
+
+int main(){
+    int n = 10;
+    std::cout<< &n << std::endl;   
+
+    int *ptr = &n;
+    std::cout<< ptr << std::endl;
+
+    std::cout<<"----------------------------------------"<<std::endl;
+
+    std::cout<< n <<std::endl;
+    std::cout<< *ptr << std::endl;
+
+return 0;
+}
